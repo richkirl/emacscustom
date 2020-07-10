e7a329bcc072fdd3d2e2b6abeefea0d523310220
richkirl <61930048+richkirl@users.noreply.github.com>
2020-07-10 12:31:45 +0300

    Delete plemconfig.sh

--- plemconfig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mcs='~/.emacs'
-mcsdir='~/.emacs.d'
-
-sudo cp $PWD/plugins.el ~/.emacs.d/
-
-echo '(setq custom-file "'$mcsdir'/plugins.el")' >> ~/.emacs
-echo '(load custom-file)' >> ~/.emacs
